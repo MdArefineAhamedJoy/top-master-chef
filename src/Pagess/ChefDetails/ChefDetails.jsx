a47edc46fd5c16d2dccdeedb51aecb0e05dbfef7
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-05-02 21:32:59 +0600

    add chef item section

--- src/Pagess/ChefDetails/ChefDetails.jsx
+++ src/Pagess/ChefDetails/ChefDetails.jsx
@@ -4,7 +4,6 @@ import ChefRecipe from "../ChefRecipe/ChefRecipe";
 
 const ChefDetails = () => {
   const details = useLoaderData();
-  console.log(details);
   const {
     chefName,
     chefPicture,
@@ -18,24 +17,30 @@ const ChefDetails = () => {
   return (
     <div className="px-5">
       <div className="card md:card-side bg-base-100 shadow-xl my-20">
-        <figure className="w-3/1">
-          <img
-            src={chefPicture}
-            alt="Album"
-          />
+        <figure className="w-3/1 p-10">
+          <img src={chefPicture} alt="Album" />
         </figure>
         <div className="card-body w-1/3">
-          <h1 className="text-center font-bold mb-5 pb-2 border-b-2 border-sky-700 text-2xl">Chef Details Information</h1>
+          <h1 className="text-center font-bold mb-5 pb-2 border-b-2 border-sky-700 text-2xl">
+            Chef Details Information
+          </h1>
           <h2 className="card-title">Chef Name : {chefName}</h2>
-          <small className="font-bold">Number Of Recipes: {   numRecipes}</small>
+          <small className="font-bold">Number Of Recipes: {numRecipes}</small>
           <small className="font-bold">People Likes: {likes}</small>
           <small className="font-bold">Experience : {yearsExperience}</small>
-          <p ><span className="font-bold">Chef Bio </span>: <br /> { bio}</p>
+          <p>
+            <span className="font-bold">Chef Bio </span>: <br /> {bio}
+          </p>
         </div>
       </div>
-      {
-        recipes.map(recipe => <ChefRecipe recipe={recipe} key={recipe.item}></ChefRecipe>)
-      }
+      <div className="grid grid-cols-1 gap-3">
+        {recipes.map((recipeItem) => (
+          <ChefRecipe
+            recipeItem={recipeItem}
+            key={recipeItem.item}
+          ></ChefRecipe>
+        ))}
+      </div>
     </div>
   );
 };
--- src/Pagess/ChefRecipe/ChefRecipe.jsx
+++ src/Pagess/ChefRecipe/ChefRecipe.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, EffectCoverflow } from "swiper";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
 
-const ChefRecipe = () => {
-    return (
-        <div className='mb-10'>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet nesciunt eum veritatis quo ullam sit corporis? Amet inventore, hic ea facilis velit ut voluptate, et necessitatibus tempora, dicta maiores.</p>
+const ChefRecipe = ({ recipeItem }) => {
+  const { imageURL, name, rating, cookingMethod, ingredients } = recipeItem;
+  console.log(recipeItem);
+  return (
+    <div className="mb-10 ">
+      <div className="card  card-side bg-base-100 shadow-xl">
+        <figure className="w-1/1">
+          <img className="w-full h-full" src={imageURL} alt="Movie" />
+        </figure>
+        <div className="card-body w-2/12">
+          <p className="font-bold text-lg">Item Name : {name}</p>
+          <p><span className="font-bold">CookingMethod :</span> {cookingMethod}</p>
+          <div className=" ">
+           <span className="font-bold ">Ingredients :</span>
+            {ingredients.map((ingredient) => (
+              <span > {ingredient} , </span>
+            ))}
+          </div>
+          <p><span className="font-bold">Rating : </span>{rating}</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Watch</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ChefRecipe;
