f45486c2f5f656395d73531c6f78edfaf97daadf
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-05-02 19:27:42 +0600

    show details card and data

--- Utilites/loader.js
+++ Utilites/loader.js
@@ -1,7 +1,7 @@
 const handelDetails = async (id) => {
-  let storeData = [];
+ 
   const chefId = id.id;
-  console.log(chefId);
+ 
 
   const response = await fetch(`http://localhost:5000/chefDetails`);
   const data = await response.json();
--- src/Pagess/ChefDetails/ChefDetails.jsx
+++ src/Pagess/ChefDetails/ChefDetails.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const ChefDetails = () => {
-    const details = useLoaderData()
- 
-    return (
-        <div>
-            {
-                   console.log(details)
-            
-            }
+  const details = useLoaderData();
+  console.log(details);
+  const {
+    chefName,
+    chefPicture,
+    likes,
+    numRecipes,
+    recipes,
+    yearsExperience,
+    id,
+    bio,
+  } = details;
+  return (
+    <div>
+      <div className="card lg:card-side bg-base-100 shadow-xl">
+        <figure className="w-3/1">
+          <img
+            src={chefPicture}
+            alt="Album"
+          />
+        </figure>
+        <div className="card-body w-1/3">
+          <h2 className="card-title">Chef Name : {chefName}</h2>
+          <small className="font-bold">Number Of Recipes: {   numRecipes}</small>
+          <small className="font-bold">People Likes: {likes}</small>
+          <small className="font-bold">Experience : {yearsExperience}</small>
+          <p>{ bio}</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Listen</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ChefDetails;
