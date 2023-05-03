becc1673514370e2a160bd7fcb46c5d534364472
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-05-03 21:46:34 +0600

    add envirment file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBnZm-U15Zj518H8tIGY2OWapb3jAVd6cU",
-  authDomain: "world-top-chef.firebaseapp.com",
-  projectId: "world-top-chef",
-  storageBucket: "world-top-chef.appspot.com",
-  messagingSenderId: "1087220983541",
-  appId: "1:1087220983541:web:c0fd4fcfe3f56fabf3c7bc"
+  apiKey:import.meta.envVITE_APIKEY,
+  authDomain:import.meta.envVITE_AUTHDOMAIN,
+  projectId:import.meta.envVITE_PROJECTID,
+  storageBucket:import.meta.envVITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.envVITE_MESSAGINGSENDERID,
+  appId:import.meta.envVITE_APPID,
 };
 
 // Initialize Firebase
